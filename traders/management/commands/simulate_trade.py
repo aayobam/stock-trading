9ffbe2b7adb1f9c31645e9f9ef614790b5f439e3
Abayomi Olowu <aayobam@gmail.com>
2023-05-25 09:07:43 +0100

    Update simulate_trade.py

--- traders/management/commands/simulate_trade.py
+++ traders/management/commands/simulate_trade.py
@@ -1,4 +1,3 @@
-from os import close
 import time
 import random
 import json, decimal
@@ -9,6 +8,7 @@ from django.core.management import BaseCommand, CommandError
 from traders.models import Trade
 
 
+# Converts instance of decimal to string
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):
@@ -16,6 +16,7 @@ class DecimalEncoder(json.JSONEncoder):
         return super().default(obj)
 
 
+# This automates the trading once the commabd is ran on thr command line
 class Command(BaseCommand):
     help = 'Simulates profit and loss for traders'
 
@@ -29,23 +30,29 @@ class Command(BaseCommand):
                 data_entries = []
                 trade.balance = Decimal(str(trade.balance))
                 profit_loss = Decimal(random.randint(-10, 10))
-
+                
+                # Excludes profile with 0 balance or balance less than thr loss from being simulated for trading
                 if trade.balance == Decimal('0.00') or trade.balance < abs(profit_loss):
                     # Trading stops for an account with zero balanace or less then the profit_loss
                     continue
-
+                
+                # excludes staff or admin profiles from trading
                 elif trade.trader.is_superuser or trade.trader.is_staff:
                     continue
                 
-                trade.balance += profit_loss # adds profit or deduct loss from account balance.
+                trade.balance += profit_loss # Adds profit or deduct loss from account balance.
                 trade_data = {
                     "trader_name": str(trade.trader),
                     "balance": trade.balance,
                     "profit_loss": profit_loss,
                     "timestamp": str(datetime.now())
                 }
+                # Converts to json data
                 trade_json = json.dumps(trade_data, cls=DecimalEncoder)
+                
+                # converts to python dictionary
                 data_list = json.loads(trade_json)
+                
                 data_entries.append(data_list)
                 trade.graph_data += data_entries 
                 trade.save()
