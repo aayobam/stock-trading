3e52bd8311d5632b45c20b1fde8ce6098d05860c
Abayomi Olowu <aayobam@gmail.com>
2023-05-25 09:16:54 +0100

    Update decorators.py

--- users/decorators.py
+++ users/decorators.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect
 
 
+# Refuses a user from accessing a page once already logged in.
 def is_authenticated(view_func):
     def wrapper_func(request, *args, **kwargs):
         user = request.user
