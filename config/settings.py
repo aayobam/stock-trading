b811fb272a78149a781ae9075dec7eef5e5155c5
Abayomi Olowu <aayobam@gmail.com>
2023-06-01 17:07:39 +0100

    configured redis on render and added link to project to be deployed on railway

--- config/settings.py
+++ config/settings.py
@@ -191,8 +191,9 @@ if DEBUG == False:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Celery settings
-CELERY_BROKER_URL = "redis://localhost:6379"
-CELERY_RESULT_BACKEND = "redis://localhost:6379"
+CELERY_BROKER_URL = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.environ.get('REDIS_URL', 'redis://localhost:6379')
+CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_AACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 
