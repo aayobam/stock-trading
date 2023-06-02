5cfd02c334b2065bcaffb37bdcd3b8ebe84e9fec
Abayomi Olowu <aayobam@gmail.com>
2023-06-02 16:04:32 +0100

    fixed dashboard errors

--- traders/urls.py
+++ traders/urls.py
@@ -6,7 +6,4 @@ urlpatterns = [
     path('user-graph', views.UserDashboardGraphView.as_view(), name="user_graph"),
     path('admin/dashboard', views.AdminDashboardTradeListView.as_view(), name="admin_dashboard"),
     path('admin-graph', views.AdminDashboardGraphView.as_view(), name="admin_graph"),
-
-    #path('admin/dashboard', views.admin_dashboard_trade_list_view, name="admin_dashboard"),
-    #path('admin-graph', views.admin_dashboard_graph_view, name="admin_graph"),
 ]
